cd6bc494387a5d976d461b0a2699a0808ac1e0b1
NNHieu <ngochieutb13@gmail.com>
2020-04-12 21:33:23 +0700

    added temp diagram

--- HomestayWeb/urls.py
+++ HomestayWeb/urls.py
@@ -1,4 +1,4 @@
-"""Homestay URL Configuration
+"""HomestayWeb URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
--- UMS/models.py
+++ UMS/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+
